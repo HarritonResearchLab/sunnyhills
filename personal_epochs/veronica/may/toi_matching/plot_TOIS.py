e5333fc46ba416b310e0a68bc895201d2eaffa6c
Vernonicuhhh <urmumalad@gmail.com>
2022-05-26 09:51:36 -0700

    made the graph nicer and saved the figure

--- personal_epochs/veronica/may/toi_matching/plot_TOIS.py
+++ personal_epochs/veronica/may/toi_matching/plot_TOIS.py
@@ -8,6 +8,11 @@ def merge_keys():
 
     merged.to_csv('merged_key.csv')
 
+def addAnnotations(x,y):
+    import matplotlib.pyplot as plt
+    for pos in range(len(x)):
+        plt.text(pos,y[pos],y[pos],ha='center')
+
 def plot_toi_percentages():
     import matplotlib.pyplot as plt
     import pandas as pd
@@ -15,21 +20,32 @@ def plot_toi_percentages():
         'APC':0,
         'CP':0,
         'FA':0,
+        'FP':0,
         'KP':0,
         'PC':0
     }
 
-    df = pd.read_csv('merged_key.csv')
-    df = df.dropna()
+    old_df = pd.read_csv('C:/Users/60002/Documents/GitHub/sunnyhills/personal_epochs/veronica/may/toi_matching/toi_export.csv')
+    df = old_df.dropna()
+    planetary_keys =['ambiguous planetary candidate', 'Confirmed Planet','False Alarm','False Positive','Known Planet','Planetary Candidate']
     for toi in df['tfopwg_disp']:
         toi_keys[str(toi)]+=1
 
     keys = list(toi_keys.keys())
     vals = list(toi_keys.values())
 
-    plt.bar(keys,vals)
+    for key,val in zip(keys,vals):
+        plt.bar(key,val)
+    plt.legend(planetary_keys,title='keys')
+    plt.xlabel('TOI Key')
+    plt.ylabel('Count')
+    plt.title('TOI Dispositions')
+    addAnnotations(keys,vals)
     plt.show()
     
+    return (len(old_df['tfopwg_disp'])-len(df['tfopwg_disp']))/len(old_df['tfopwg_disp'])*100
+    
 
-
+count = plot_toi_percentages()
+print(count)
     
