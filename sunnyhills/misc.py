9f0a346d087c806d48353b576a12388fc5a832a1
RyanAxe <97776863+RyanAxe@users.noreply.github.com>
2022-07-02 17:53:21 -0700

    Update misc.py

--- sunnyhills/misc.py
+++ sunnyhills/misc.py
@@ -306,48 +306,7 @@ def merge_plots(tic_id:str='',plot_dir:str='routines/alpha_tls/plots/',export_di
     print('successfully generated: ' + tic_id + ' report!')
 
 
-def return_kerr_cluster(gaiaid: str):
-    names=['Cepheus Flare','Pleiades','Taurus-Orion','Ophiuchus Southeast','Fornax-Horologium','CMa North','Aquila East','Cepheus Far North','Vela-CG7','ASCC 123','Cepheus-Cygnus','Lyra','Cerberus','Carina-Musca','Perseus','Perseus','Taurus-Orion II','Greater Taurus','IC 2391	101','NGC 2451A','Chamaeleon','Sco-Cen	7394','Taurus-Orion III','Vela-CG4','Taurus-Orion IV','Monoceros Southwest','Greater Orion']
     #note for some reason when making this csv with excel pandas returned a dtype warning. To fix this just specify the dtype as unicode
-    df1=pd.read_csv('./personal_epochs/ryan/Summer/Week3/kerr context/table1.csv',dtype='unicode')
-    df2=pd.read_csv('./personal_epochs/ryan/Summer/Week3/kerr context/table2.csv',dtype='unicode')
-
-    id_table1=np.array(df1['GAIA ID'])
-    EOM_table1=np.array(df1['EOM'])
-    TLC_table1=np.array(df1['TLC'])
-    id_table2=np.array(df2['GAIA ID'])
-    EOM_table2=np.array(df2['EOM'])
-    TLC_table2=np.array(df2['TLC'])
-    breakcheck='start'
-    cluster_name=0
-    x=0
-    #use str() input again to make sure gaiaid is infact a string. Might be redundant...
-    match_id1=np.where(str(gaiaid)==id_table1)[0]
-    match_id2=np.where(str(gaiaid)==id_table2)[0]
-    #Use .any() to remove python warnings... Function still produces accurate results.
-        
-    if(EOM_table2[match_id2].any()=='-1' and TLC_table2[match_id2].any()!='-1'):
-        x=TLC_table2[match_id2]
-        cluster_name=names[int(x)]
-        breakcheck='if_loop1'
-    elif(EOM_table1[match_id1].any()=='-1' and TLC_table1[match_id1].any()!='-1'):
-        x=TLC_table1[match_id1]
-        cluster_name=names[int(x)]
-        breakcheck='elif_loop1'
-    ###################################################################################################
-    elif(EOM_table2[match_id2].any()=='-1' and TLC_table1[match_id1].any()=='-1'):
-        cluster_name='Field Star'
-        breakcheck='elif_loop2'
-    elif(EOM_table1[match_id1].any()=='-1' and TLC_table1[match_id1].any()=='-1'):
-        cluster_name='Field Star'
-        breakcheck='elif_loop3'
-    ########################################################################################################
-    else:
-        cluster_name='NOT FOUND'
-        breakcheck='else_loop1'
-        
-    #can return and print breakcheck for debugging reasons...
-    return cluster_name
 
 def find_data_gap(array:np.array):
     '''
