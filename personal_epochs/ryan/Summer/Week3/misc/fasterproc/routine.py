10167471a9e9a6c88825aa07b56e8900d3e2b377
RyanAxe <97776863+RyanAxe@users.noreply.github.com>
2022-07-04 15:07:56 -0700

    Updates....

--- personal_epochs/ryan/Summer/Week3/misc/fasterproc/routine.py
+++ personal_epochs/ryan/Summer/Week3/misc/fasterproc/routine.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pandas as pd
+import os
+import time
+dirindex=os.listdir()
+dirindex=dirindex.pop('routine.py')
+print(dirindex)
+input('Correct dir listing?')
 masterdf=pd.DataFrame()
 TIC_index=[]
+
+
 def gaia_to_tic(gaia_ids):
     from astrobase.services.identifiers import gaiadr2_to_tic
     tic_ids = []
@@ -19,11 +27,18 @@ def gaia_to_tic(gaia_ids):
 
 
     return tic_ids
+print('loaded function')
 
 
+for i in dirindex:
+    df=pd.read_csv(i)
+    print('csv read')
+    GAIA_index=df['GAIA']
+    x=gaia_to_tic(GAIA_index)
+    TIC_index.append(x)
+    print('output appended and sleeping for 1 minute...')
+    time.sleep(60)
 
-df=pd.read_csv('dummy.csv')
-GAIA_index=df['GAIA']
-x=gaia_to_tic(GAIA_index)
-masterdf['TIC_ID']=x
+print('Finishing up...')
+masterdf['TIC_ID']=TIC_index
 masterdf.to_csv('output')
