482c7b8f3ff9765bae6b7a968db86614b1fe7d79
RyanAxe <97776863+RyanAxe@users.noreply.github.com>
2022-07-04 11:46:37 -0700

    Update routine.py

--- personal_epochs/ryan/Summer/Week3/misc/routine.py
+++ personal_epochs/ryan/Summer/Week3/misc/routine.py
@@ -1,5 +1,26 @@
 import numpy as np
 import pandas as pd
 
+
+def gaia_to_tic(gaia_ids):
+    from astrobase.services.identifiers import gaiadr2_to_tic
+    tic_ids = []
+    for gaia_id in gaia_ids:
+        print(gaia_id)
+        try: 
+            if gaia_id !=None:
+                tic_ids.append(str(gaiadr2_to_tic(gaia_id)))
+
+            else: 
+                tic_ids.append('')
+        except: 
+            tic_ids.append('')
+            continue 
+
+
+    return dict(zip(gaia_ids, tic_ids)), tic_ids
+
+
+
 df=pd.read_csv('kerr1.csv')
 GAIA_index=df['GAIA']
