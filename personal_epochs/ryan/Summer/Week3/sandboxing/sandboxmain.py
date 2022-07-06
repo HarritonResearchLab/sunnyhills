8a1fe435d7a167d4793f58ab74ec4446023874fc
RyanAxe <97776863+RyanAxe@users.noreply.github.com>
2022-07-06 14:16:03 -0700

    Update sandboxmain.py

--- personal_epochs/ryan/Summer/Week3/sandboxing/sandboxmain.py
+++ personal_epochs/ryan/Summer/Week3/sandboxing/sandboxmain.py
@@ -9,7 +9,7 @@ GAIA_index=[]
 def gaia_to_tic(gaia_ids):
     from astrobase.services.identifiers import gaiadr2_to_tic
     tic_ids = []
-    tic_ids.append(str(gaiadr2_to_tic(gaia_ids)))
+    tic_ids.append(gaiadr2_to_tic(gaia_ids))
     '''
     for gaia_id in gaia_ids:
         print(gaia_id)
@@ -23,17 +23,44 @@ def gaia_to_tic(gaia_ids):
             tic_ids.append('')
             continue 
     '''
-
     return tic_ids
 print('loaded function')
 
 df=pd.read_csv('kerr1.csv')
 GAIA_index=df['GAIA']
 
-pool=Pool(8)
+pool=Pool(16)
 TIC_index=pool.map(gaia_to_tic, GAIA_index)
 
 
 print('Finishing up...')
 masterdf['TIC_ID']=TIC_index
 masterdf.to_csv('output')
+
+
+'''
+multiprocessing.pool.RemoteTraceback:
+
+Traceback (most recent call last):
+  File "/home/fjuhsd/miniconda3/envs/py37/lib/python3.7/multiprocessing/pool.py", line 121, in worker
+    result = (True, func(*args, **kwds))
+  File "/home/fjuhsd/miniconda3/envs/py37/lib/python3.7/multiprocessing/pool.py", line 44, in mapstar
+    return list(map(*args))
+  File "sandboxmain.py", line 12, in gaia_to_tic
+    tic_ids.append(str(gaiadr2_to_tic(gaia_ids)))
+  File "/home/fjuhsd/miniconda3/envs/py37/lib/python3.7/site-packages/astrobase/services/identifiers.py", line 355, in gaiadr2_to_tic
+    return str(matched_tic_id.item())
+ValueError: can only convert an array of size 1 to a Python scalar
+
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "sandboxmain.py", line 34, in <module>
+    TIC_index=pool.map(gaia_to_tic, GAIA_index)
+  File "/home/fjuhsd/miniconda3/envs/py37/lib/python3.7/multiprocessing/pool.py", line 268, in map
+    return self._map_async(func, iterable, mapstar, chunksize).get()
+  File "/home/fjuhsd/miniconda3/envs/py37/lib/python3.7/multiprocessing/pool.py", line 657, in get
+    raise self._value
+ValueError: can only convert an array of size 1 to a Python scalar
+'''
